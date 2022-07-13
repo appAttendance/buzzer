79de3506c71f1b5cd7c17b3908c075ac919c57fc
Eloy Degen <degeneloy@gmail.com>
2022-07-13 21:36:48 +0200

    Add new stuff for Hacker Jeopardy!

--- public/host.js
+++ public/host.js
@@ -2,6 +2,10 @@ const socket = io()
 const active = document.querySelector('.js-active')
 const buzzList = document.querySelector('.js-buzzes')
 const clear = document.querySelector('.js-clear')
+document.body.style.backgroundColor = "white";
+
+
+var cleaned = false;
 
 socket.on('active', (numberActive) => {
   active.innerText = `${numberActive} joined`
@@ -15,9 +19,22 @@ socket.on('buzzes', (buzzes) => {
     })
     .map(user => `<li>${user.name} on Team ${user.team}</li>`)
     .join('')
+    // const element = document.getElementsByClassName("body")
+    //element[0].style["background-color"] = "yellow";
+
+
+    if (cleaned === false){
+    document.body.style.backgroundColor = "red";
+    }
+    // This seems to be needed, but I'm very confused
+    cleaned = false;
+
 })
+
+
 
 clear.addEventListener('click', () => {
+  document.body.style.backgroundColor = "white";
+  cleaned = true;
   socket.emit('clear')
 })
-
